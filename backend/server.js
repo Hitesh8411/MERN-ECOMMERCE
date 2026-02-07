0f0d339971d5b7b02916cba62343646082cd4b91
Hitesh8411 <hiteshsuryavanshi2004@gmail.com>
2026-02-07 11:40:05 +0530

    Product Management (Till Backend only

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-
-
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./models/product.js";
 
 dotenv.config();
 
@@ -12,14 +11,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send('API is running...');
-// });
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 connectDB();
 
 app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+  console.log("Server is running on port 5001");
 });
