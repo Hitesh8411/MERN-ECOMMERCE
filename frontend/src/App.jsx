6271f4996fa8f062182882fde98d0562e09be287
Hitesh8411 <hiteshsuryavanshi2004@gmail.com>
2026-02-08 11:58:28 +0530

    Add to cart , Update and total

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,36 @@ import ProductDetails from "./pages/ProductDetails";
 import ProductList from "./admin/ProductList";
 import AddProduct from "./admin/AddProduct";
 import EditProduct from "./admin/EditProduct";  
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router";
+import Cart from "./pages/Cart";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
-  
+
+  {   element: <Layout />, children: [
+
       { path: "/", element: <Home /> },
       { path: "/login", element: <Login /> },
       { path: "/signup", element: <Signup /> },
       { path: "/product/:id", element: <ProductDetails /> },
+      { path: "/cart", element: <Cart /> },
+
       { path: "/admin/products", element: <ProductList /> },
       { path: "/admin/products/add", element: <AddProduct /> },
       { path: "/admin/products/edit/:id", element: <EditProduct /> },
-      // Add more routes as needed
-  
+      
+
+  ],  
+  }
   
 ]);
 
